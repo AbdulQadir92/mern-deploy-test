32563d395fdf7e840ef41077b7f76a1927af9734
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-09-24 16:08:26 +0500

    connected mongodb database to the app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const path = require('path');
 
 app.get('/api', (req, res) => {
@@ -14,13 +14,19 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
+} else {
+    // for development purpose
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
 }
 
-app.use(express.static('frontend/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`mongodb database connected and server running on port ${process.env.PORT || 4000}`);
+        })
+    })
+    .catch(error => console.log(error))
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log('server running on port 4000')
-})
